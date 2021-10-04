d55f6d83e506ccc339035c3c0eab93c3a12555fb
Jaehyeong Ha <cubeprince@gmail.com>
2021-10-04 14:46:43 +0900

    save and find member test

---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,27 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberRepositoryTest {
+@Autowired MemberRepository memberRepository;
+    @Test
+    public void testMember(){
+        Member member = new Member(null, "memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(savedMember.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(savedMember);
+    }
+}
