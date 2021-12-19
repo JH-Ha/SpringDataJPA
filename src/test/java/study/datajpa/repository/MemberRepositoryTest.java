f2221afb173599a0dcc9448c48f0f441c666e779
Jaehyeong Ha <jaehyeong.ha12@gmail.com>
2021-12-19 21:57:23 +0900

    test memberRepository works as same as JpaRepository

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,13 +8,19 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Transactional
 @Rollback(false)
 class MemberRepositoryTest {
-@Autowired MemberRepository memberRepository;
+    @Autowired
+    MemberRepository memberRepository;
+
     @Test
-    public void testMember(){
+    public void testMember() {
         Member member = new Member("memberA");
         Member savedMember = memberRepository.save(member);
 
@@ -23,5 +29,29 @@ class MemberRepositoryTest {
         Assertions.assertThat(findMember.getId()).isEqualTo(savedMember.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
         Assertions.assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        Long count = memberRepository.count();
+        assertThat(count).isEqualTo(0);
     }
 }
